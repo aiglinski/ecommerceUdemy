dac994538f822ede016e9ff031dd0b1a3f1585fd
Andrew <aiglinski@icloud.com>
2019-11-07 22:09:14 -0600

    just pushing before I risk fucking everything up

--- src/Pages/Homepage/homepage.components.jsx
+++ src/Pages/Homepage/homepage.components.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import './homepage.styles.scss'
+import Directory from '../../Components/Directory/directory.component'
 
 const HomePage = () => (
     <div className='homepage'>
